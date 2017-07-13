6c6ef592c1854cf9bc5fddc0c06c33affd1dbdc5
Graham Ollis <plicease@cpan.org>
2017-07-13 10:52:07 +0000

    Initial commit

---
+++ lib/Alien/Build/Plugin/Fetch/Rewrite.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Plugin::Fetch::Rewrite;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Alien::Build plugin to rewrite network requests to local resources
+# VERSION
+
+1;
---
+++ t/alien_build_plugin_fetch_rewrite.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use Alien::Build::Plugin::Fetch::Rewrite;
+
+ok 1, 'todo';
+
+done_testing
